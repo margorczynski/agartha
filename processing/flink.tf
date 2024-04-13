6e08740af2d6547e179f41acc99447a602234543
Marcin Gorczyński <margorczynski@gmail.com>
2024-04-13 02:42:20 +0200

    Added Flink operator

---
+++ processing/flink.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "flink_operator" {
+  namespace  = local.flink_namespace
+  name       = "flink"
+  repository = "https://downloads.apache.org/flink/flink-kubernetes-operator-1.8.0"
+  chart      = "flink-kubernetes-operator"
+
+  set_list {
+    name  = "watchNamespaces"
+    value = [ local.flink_namespace ]
+  }
+
+  set {
+    name  = "webhook.create"
+    value = "true"
+  }
+}
